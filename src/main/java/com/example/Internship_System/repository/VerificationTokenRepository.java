efc88b14287240a53e8fb04c958d7c1e7a6b8947
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-11-04 20:36:23 +0700

    feat(Email-Verification): thêm api để gửi email xác nhận cho intern, nhớ thay username ,password thành email và password(app password ko phải password của email) thành của mình

---
+++ src/main/java/com/example/Internship_System/repository/VerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.Internship_System.repository;
+
+import com.example.Internship_System.auth.entity.VerificationToken;
+import com.example.Internship_System.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+    Optional<VerificationToken> findByUser(User user);
+}
