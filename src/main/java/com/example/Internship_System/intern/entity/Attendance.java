16568b587cd833b0a8c95b04a796b37341cc68b7
hnhdang <hdang4747@gmail.com>
2025-11-05 11:52:29 +0700

    feat: Attendance entity

--- src/main/java/com/example/Internship_System/intern/entity/Attendance.java
+++ src/main/java/com/example/Internship_System/intern/entity/Attendance.java
@@ -1,4 +1,79 @@
 package com.example.Internship_System.intern.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.validator.internal.util.privilegedactions.LoadClass;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "attendance")
 public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id", nullable = false)
+    private int attendanceId;
+
+    @Column(name = "intern_id")
+    private int internId;
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "check_in")
+    private LocalDateTime checkIn;
+
+    @Column(name = "check_out")
+    private LocalDateTime checkOut;
+
+    @Column(name = "location")
+    private String location;
+
+    public int getAttendanceId() {
+        return attendanceId;
+    }
+
+    public void setAttendanceId(int attendanceId) {
+        this.attendanceId = attendanceId;
+    }
+    public int getInternId() {
+        return internId;
+    }
+
+    public void setInternId(int internId) {
+        this.internId = internId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDateTime checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDateTime getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDateTime checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
