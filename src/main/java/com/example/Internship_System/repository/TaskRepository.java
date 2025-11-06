3be79d48a2715f5ab5bf62496542b68f10bbf502
hnhdang <hdang4747@gmail.com>
2025-11-06 10:17:26 +0700

    feat: task repo + services

--- src/main/java/com/example/Internship_System/repository/TaskRepository.java
+++ src/main/java/com/example/Internship_System/repository/TaskRepository.java
@@ -1,4 +1,14 @@
 package com.example.Internship_System.repository;
 
-public class TaskRepository {
+import com.example.Internship_System.task.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+Optional<Task> findTaskByMentorId(int mentorId);
+Optional<Task> findTaskByInternId(int internId);
+
 }
