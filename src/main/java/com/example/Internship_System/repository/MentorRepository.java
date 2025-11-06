f3ff56c40afcfc1787a0ee15962e792ebc352af1
Chu Mạnh Cường <cuongsdbgvip13@gmail.com>
2025-11-07 06:49:17 +0700

    feat(mentor): Tạo lớp MentorRepository để thao tác dữ liệu mentor

--- src/main/java/com/example/Internship_System/repository/MentorRepository.java
+++ src/main/java/com/example/Internship_System/repository/MentorRepository.java
@@ -1,4 +1,10 @@
 package com.example.Internship_System.repository;
 
-public class MentorRepository {
+import com.example.Internship_System.mentor.entity.MentorUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MentorRepository extends JpaRepository<MentorUser, Long> {
+    boolean existsByUser_UserId(Long userId);
 }
