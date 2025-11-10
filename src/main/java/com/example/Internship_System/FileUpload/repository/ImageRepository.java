8cc11927e6000782cd139b9d50ca063e0e31c5b0
hnhdang <hdang4747@gmail.com>
2025-11-10 10:26:02 +0700

    feat: file upload entity, service, repo

--- src/main/java/com/example/Internship_System/FileUpload/repository/ImageRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.Internship_System.FileUpload.repository;
-
-import com.example.Internship_System.FileUpload.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
