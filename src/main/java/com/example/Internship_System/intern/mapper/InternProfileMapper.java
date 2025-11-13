43cf6829ec6925230a9af20867a7116b66da5e2b
hnhdang <hdang4747@gmail.com>
2025-11-13 09:32:07 +0700

    fix: bỏ cvPath trong DTO

--- src/main/java/com/example/Internship_System/intern/mapper/InternProfileMapper.java
+++ src/main/java/com/example/Internship_System/intern/mapper/InternProfileMapper.java
@@ -10,9 +10,6 @@ import java.util.List;
 public class InternProfileMapper {
     public static InternProfileDTO toDTO(InternProfile intern, User user) {
         List<String> documents = new ArrayList<>();
-        if (intern.getCvFile() != null && !intern.getCvFile().isEmpty()) {
-            documents.add(intern.getCvFile());
-        }
         if (intern.getCvPath() != null && !intern.getCvPath().isEmpty()) {
             documents.add(intern.getCvPath());
         }
