68aa42fbb22d7dda3e7471f43e0242d5d5df875e
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-11-13 23:35:53 +0700

    feat(mentor-assignment):thêm service + DTO request của người dùng cho Mentor Assignment

---
+++ src/main/java/com/example/Internship_System/hr/dto/MentorAssignmentRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.Internship_System.hr.dto;
+
+public class MentorAssignmentRequestDTO {
+    private Integer mentorId;
+    private Integer internId;
+
+    public Integer getMentorId() { return mentorId; }
+    public void setMentorId(Integer mentorId) { this.mentorId = mentorId; }
+
+    public Integer getInternId() { return internId; }
+    public void setInternId(Integer internId) { this.internId = internId; }
+
+
+}
