33fe5b305bba332a306db6c236650c0b70998586
ChuManhCuong312 <146830932+ChuManhCuong312@users.noreply.github.com>
2025-11-14 09:06:13 +0700

    Revert "CRUD cơ bản cho phân công mentor"

--- src/main/java/com/example/Internship_System/repository/MentorAssignmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Internship_System.repository;
-
-import com.example.Internship_System.hr.entity.MentorAssignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MentorAssignmentRepository extends JpaRepository<MentorAssignment, Integer> {
-}
