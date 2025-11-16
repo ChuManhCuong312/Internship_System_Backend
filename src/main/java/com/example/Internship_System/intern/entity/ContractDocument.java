4fc5bad0b15d87844e45499b7cf6b9a81da8983c
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-11-17 01:22:45 +0700

    feat(mentor-assignment):tạo ContractDocument entity + enum cho 2 status InternConfirmStatus và ContractStatus

---
+++ src/main/java/com/example/Internship_System/intern/entity/ContractDocument.java
@@ -0,0 +1,68 @@
+package com.example.Internship_System.intern.entity;
+
+import com.example.Internship_System.auth.entity.UserStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "contract_documents")
+public class ContractDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "document_id")
+    private Integer documentId;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "intern_id", nullable = false)
+    private InternProfile intern;
+
+    @Size(max = 255, message = "File path name must not exceed 255 characters")
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "contract_status", columnDefinition = "ENUM('NOT_UPLOAD','UPLOAD') DEFAULT 'NOT_UPLOAD'")
+    private ContractStatus contractStatus = ContractStatus.NOT_UPLOAD;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "intern_confirm_status", columnDefinition = "ENUM('APPROVED','PENDING') DEFAULT 'PENDING'")
+    private InternConfirmStatus internConfirmStatus = InternConfirmStatus.PENDING;
+
+    @Column(name = "confirm_at")
+    private LocalDateTime confirmAt;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
+
+    public ContractDocument() {}
+
+    public ContractDocument (InternProfile intern, String filePath, LocalDateTime confirmAt, String note){
+        this.intern = intern;
+        this.filePath = filePath;
+        this.confirmAt = confirmAt;
+        this.note = note;
+    }
+
+    public Integer getDocumentId() {return documentId;}
+    public void setDocumentId(Integer documentId) {this.documentId = documentId;}
+
+    public InternProfile getIntern() {return intern;}
+    public void setIntern(InternProfile intern) {this.intern = intern;}
+
+    public String getFilePath() {return filePath;}
+    public void setFilePath(String filePath) {this.filePath = filePath;}
+
+    public ContractStatus getContractStatus() {return contractStatus;}
+    public void setContractStatus(ContractStatus contractStatus) {this.contractStatus = contractStatus;}
+
+    public InternConfirmStatus getInternConfirmStatus() {return internConfirmStatus;}
+    public void setInternConfirmStatus(InternConfirmStatus internConfirmStatus) {this.internConfirmStatus = internConfirmStatus;}
+
+    public LocalDateTime getConfirmAt() {return confirmAt;}
+    public void setConfirmAt(LocalDateTime confirmAt) {this.confirmAt = confirmAt;}
+
+    public String getNote() {return note;}
+    public void setNote(String note) {this.note = note;}
+}
