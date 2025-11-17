06276333d58bdcabb2ee60058ab6741be1479cc1
codesomethinguseful <hdang4747@gmail.com>
2025-11-17 23:24:26 +0700

    feat: tạo cấu trúc cho contract-api

--- src/main/java/com/example/Internship_System/intern/entity/InternConfirmStatus.java
+++ src/main/java/com/example/Internship_System/intern/entity/InternConfirmStatus.java
@@ -1,6 +1,21 @@
 package com.example.Internship_System.intern.entity;
 
 public enum InternConfirmStatus {
-    APPROVED,
-    PENDING
+    PENDING("Pending"),
+    APPROVED("Approved");
+
+    private final String displayName;
+
+    InternConfirmStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return this.displayName;
+    }
 }
