0ce2ef74915571fa299b391a15aee0cc01df32a9
codesomethinguseful <hdang4747@gmail.com>
2025-11-22 14:06:35 +0700

    feat: allowance controller

--- src/main/java/com/example/Internship_System/allowance/service/AllowanceService.java
+++ src/main/java/com/example/Internship_System/allowance/service/AllowanceService.java
@@ -1,4 +1,34 @@
 package com.example.Internship_System.allowance.service;
 
+import com.example.Internship_System.allowance.entity.Allowance;
+import com.example.Internship_System.repository.AllowanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AllowanceService {
+    @Autowired
+    private AllowanceRepository repository;
+
+    public Allowance save(Allowance allowance) {
+        return repository.save(allowance);
+    }
+
+    public List<Allowance> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Allowance> findById(int id) {
+        return repository.findById(id);
+    }
+
+    public List<Allowance> findByInternId(int internId) {
+        return repository.findByInternId(internId);
+    }
+
+    public void deleteById(int id) {
+        repository.deleteById(id);
+    }
 }
