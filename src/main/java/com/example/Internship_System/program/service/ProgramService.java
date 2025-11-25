44e002de15c15094e3922ed84ac28cadd21fdb6e
XuanThanh05 <xuanthanhle999@gmail.com>
2025-11-25 21:58:00 +0700

    thêm api calendar

--- src/main/java/com/example/Internship_System/program/service/ProgramService.java
+++ src/main/java/com/example/Internship_System/program/service/ProgramService.java
@@ -212,7 +212,7 @@ public class ProgramService {
         // START
         events.add(new ScheduleEventDTO(
                 program.getProgramId() + "-start",
-                program.getName(),
+                "Bắt đầu " +program.getName(),
                 "Bắt đầu " + program.getName(),
                 "program",
                 program.getStartDate(),
@@ -222,7 +222,7 @@ public class ProgramService {
         // END
         events.add(new ScheduleEventDTO(
                 program.getProgramId() + "-end",
-                program.getName(),
+                "Kết thúc " +program.getName(),
                 "Kết thúc " + program.getName(),
                 "program",
                 program.getEndDate(),
