d0c63e515e943a2b3386c64352396773c12a4151
hnhdang <hdang4747@gmail.com>
2025-11-28 09:07:47 +0700

    feat: filter cho tasks

--- src/main/java/com/example/Internship_System/repository/TaskRepository.java
+++ src/main/java/com/example/Internship_System/repository/TaskRepository.java
@@ -2,15 +2,33 @@ package com.example.Internship_System.repository;
 
 import com.example.Internship_System.task.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
-    Optional<Task> findTaskByMentorId(int mentorId);
-    List<Task> findByTaskIdIn(List<Integer> ids); // task của team
+    Optional<Task> findByTaskId(int taskId);
     List<Task> findByMentorId(int mentorId);
     List<Task> findByProgramId(int programId);
+    List<Task> findByTaskIdIn(List<Integer> ids); // task của team
 
+    @Query("SELECT t FROM Task t WHERE " +
+            "(:mentorId IS NULL OR t.mentorId = :mentorId) AND " +
+            "(:programId IS NULL OR t.programId = :programId) AND " +
+            "(:status IS NULL OR t.status = :status) AND " +
+            "(:priority IS NULL OR t.priority = :priority) AND " +
+            "(:startDate IS NULL OR t.created_at >= :startDate) AND " +
+            "(:endDate IS NULL OR t.created_at <= :endDate)")
+    List<Task> filterTasks(
+            @Param("mentorId") Integer mentorId,
+            @Param("programId") Integer programId,
+            @Param("status") String status,
+            @Param("priority") String priority,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 }
