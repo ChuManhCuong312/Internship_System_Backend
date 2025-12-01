2ef653f697406b1b2aa82a08777edcd05b1f91e3
hnhdang <hdang4747@gmail.com>
2025-12-01 10:13:06 +0700

    feat: lombok, unused entity trong TaskAssignment

--- src/main/java/com/example/Internship_System/task/entity/TaskTeamAssignment.java
+++ src/main/java/com/example/Internship_System/task/entity/TaskTeamAssignment.java
@@ -1,7 +1,11 @@
 package com.example.Internship_System.task.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "task_team_assignments")
 public class TaskTeamAssignment {
@@ -9,16 +13,6 @@ public class TaskTeamAssignment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private Integer taskId;
     private Integer teamId;
-
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-
-    public Integer getTaskId() { return taskId; }
-    public void setTaskId(Integer taskId) { this.taskId = taskId; }
-
-    public Integer getTeamId() { return teamId; }
-    public void setTeamId(Integer teamId) { this.teamId = teamId; }
 }
