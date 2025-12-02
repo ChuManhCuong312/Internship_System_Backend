6beff78ff22512e16906385d4bdeae00fcced3e1
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-12-03 05:03:35 +0700

    feature(ProgramAndTeam): thêm query tìm thực tập sinh chưa được phân công nhưng đã ký hợp đồng + querry kiểm tra tts đã được phân vào chương trình hay team chưa

--- src/main/java/com/example/Internship_System/repository/TeamInternRepository.java
+++ src/main/java/com/example/Internship_System/repository/TeamInternRepository.java
@@ -1,5 +1,6 @@
 package com.example.Internship_System.repository;
 
+import com.example.Internship_System.intern.entity.InternProfile;
 import com.example.Internship_System.team.entity.TeamIntern;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -25,4 +26,35 @@ public interface TeamInternRepository extends JpaRepository<TeamIntern, Integer>
     Integer findTeamIdByInternId(@Param("internId") Integer internId);
     @Query("SELECT ti FROM TeamIntern ti WHERE ti.intern.internId = :internId")
     TeamIntern findByInternId(@Param("internId") Integer internId);
+
+    boolean existsByTeamTeamIdAndInternInternId(Integer teamId, Integer internId);
+
+    @Query("""
+    SELECT i
+    FROM InternProfile i
+    JOIN ContractDocument c ON c.intern.internId = i.internId
+    LEFT JOIN TeamIntern ti ON ti.intern.internId = i.internId
+    JOIN User u ON i.userId = u.userId
+    WHERE u.fullName LIKE %:keyword%
+      AND c.internConfirmStatus = com.example.Internship_System.intern.entity.InternConfirmStatus.APPROVED
+      AND u.status = com.example.Internship_System.auth.entity.UserStatus.ACTIVE
+      AND ti.team IS NULL
+""")
+    List<InternProfile> searchAvailableInterns(@Param("keyword") String keyword);
+
+    @Query("""
+    SELECT COUNT(ti) > 0
+    FROM TeamIntern ti
+    JOIN ti.team t
+    WHERE ti.intern.internId = :internId
+      AND t.program.programId = :programId
+""")
+    boolean isInternInProgram(int internId, int programId);
+
+    @Query("""
+    SELECT COUNT(ti) > 0
+    FROM TeamIntern ti
+    WHERE ti.intern.internId = :internId
+""")
+    boolean isInternInAnyTeam(int internId);
 }
