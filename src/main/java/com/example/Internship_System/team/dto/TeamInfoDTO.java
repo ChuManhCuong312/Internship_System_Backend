419a160d93ab429597b5ac1142fff47da98f3e43
Justroamming <148474299+Justroamming@users.noreply.github.com>
2025-12-03 05:06:22 +0700

    feature(ProgramAndTeam): sửa lại dto thêm mentor id + thêm api  tìm tts còn chưa được phân công, api thêm tts vào team, api tìm tất cả tts trong 1 team

--- src/main/java/com/example/Internship_System/team/dto/TeamInfoDTO.java
+++ src/main/java/com/example/Internship_System/team/dto/TeamInfoDTO.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 public class TeamInfoDTO {
     private Integer teamId;
+    private Integer mentorId;
     private String mentorName;
     private List<TeamInternInfoDTO> interns;
 
-    public TeamInfoDTO(Integer teamId, String mentorName, List<TeamInternInfoDTO> interns) {
+    public TeamInfoDTO(Integer teamId, Integer mentorId,String mentorName, List<TeamInternInfoDTO> interns) {
         this.teamId = teamId;
+        this.mentorId = mentorId;
         this.mentorName = mentorName;
         this.interns = interns;
     }
 
     public Integer getTeamId() {return teamId;}
     public void setTeamId(Integer teamId) {this.teamId = teamId;}
+
+    public Integer getMentorId() {return mentorId;}
+    public void setMentorId(Integer mentorId) {this.mentorId = mentorId;}
 
     public String getMentorName() {return mentorName;}
     public void setMentorName(String mentorName) {this.mentorName = mentorName;}
