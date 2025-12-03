36f7cb19cb7aad70374b5f2c0b066d34720ae31b
Justroamming <lehung050707@gmail.com>
2025-12-03 08:40:01 +0700

    feature(ProgramAndTeam): sửa lại dto để cập nhật phòng ban của chương trình

--- src/main/java/com/example/Internship_System/program/dto/ProgramUpdateRequest.java
+++ src/main/java/com/example/Internship_System/program/dto/ProgramUpdateRequest.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 public class ProgramUpdateRequest {
 
     private String name;
+    private String department;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private String detail;
@@ -13,6 +14,9 @@ public class ProgramUpdateRequest {
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    public String getDepartment() {return department;}
+    public void setDepartment(String department) {this.department = department;}
 
     public LocalDateTime getStartDate() { return startDate; }
     public void setStartDate(LocalDateTime startDate) { this.startDate = startDate; }
